b88b36d77eb76b7dd50aa0b881e0c037beeb62a2
joydipta90 <mailtojoydipta@gmail.com>
2024-08-20 22:01:58 +0530

    Create Jenkins_on_RaspberryPi.sh

---
+++ Jenkins_on_RaspberryPi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install openjdk-8-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
